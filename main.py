7bc84958ce57068c2206253550da2b280b3a873f
vaibhavvm2005 <vaibhavmadival331@gmail.com>
2024-06-14 18:24:14 +0530

    completed

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-import random
-user_wins = 0
-computer_wins = 0
+print("welcome to my general knowledge quiz!")
+playing = input("Do you want to play?")
+if playing != "yes":
+    quit()
+print("okay! let's play")
 
-options = ["rock","paper","scissors"]
-while True:
-    user_input = input("type Rock/paper/scissors or Q to quit:").lower()
-    if user_unput == "q":
-        break
-    if user_input not in option:
-        continue
-    random_number = random.randit(0,2)
-    computer_pick = options
+score = 0
+answer1 = input("what does cpu stands for?")
+if answer1 == "central processing unit":
+    print('correct!')
+    score1 = score + 1
+    print("Your Score:",score1)
+else:
+    print('incorrect')
+    score1 = score - 1
+    print("Your Score:",score1)
+answer2 = input(" what does gpu stands for?")
+if answer2 == "graphing processing unit":
+    print('correct!')
+    score2 = score + 1
+    print("Your Score:",score2)
+else:
+    print('incorrect')
+    score2 = score - 1
+    print("Your Score:",score2)
+score=score1 + score2
+print("score=",score)
